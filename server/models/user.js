e911eecc8d9c1dce194fd9d7fdb2af4c31c8c39b
melih topraksever <melihtopraksever@AC9402CA.ipt.aol.com>
2018-11-02 12:32:11 +0300

    logout

--- server/models/user.js
+++ server/models/user.js
@@ -97,6 +97,15 @@ UserSchema.statics.findByCredentials = function (email, password) {
     });
   });
 };
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
 
 
 
