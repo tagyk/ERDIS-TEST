5c125863f79c0c9194d8979162088ad265ba666c
melih topraksever <melihtopraksever@AC9402CA.ipt.aol.com>
2018-11-05 17:44:25 +0300

    mssql client added

---
+++ mssql-client/getData-ShippingReceipt.js
@@ -0,0 +1,35 @@
+var sql = require("mssql");
+var webconfig = {
+    server: "erktrdepos03",
+    user: "ax",
+    password: "465666",
+    database: 'AXATAWM_CL',
+    connectionString: "Driver={SQL Server Native Client 11.0};Server=#{server}\\sql;Database=#{database};Uid=#{user};Pwd=#{password};",
+    options: {
+        trustedConnection: false,
+        encrypt: false
+    }
+
+
+};
+
+sql.connect(webconfig, function (err) {
+
+    if (err) console.log(err);
+
+    // create Request object
+    var request = new sql.Request();
+    // query to the database and get the records
+    request.query("select TOP (10)* from CancelledOrders", function (err, result) {
+
+        if (err) console.log(err)
+        console.log(result.rowsAffected[0]);
+        console.log(result.recordset[0].MNUCODE);
+
+        console.dir(result);
+
+    });
+});
+
+
+
