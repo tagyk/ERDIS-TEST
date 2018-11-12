de47c51ac238fe4192ffa62e7c032e81306c4398
Melih Topraksever <melihtopraksever@hotmail.com>
2018-11-12 21:22:51 +0300

    asd

--- mssql-client/getData-ShippingReceipt_V2.js
+++ mssql-client/getData-ShippingReceipt_V2.js
@@ -1,47 +1,15 @@
-const sql = require("mssql");
-const fs = require("fs");
-
-class shippingReceiptModel {
-
-
-    constructor(_documentNum, _documentDate) {
-        this.documentNum = _documentNum;
-        this.documentDate = _documentDate;
-        this.boxHeaders = new Array();
-    };
-    addboxHeaders(_boxHeaderModel) {
-        // this.boxHeaders = [];
-        this.boxHeaders.push(_boxHeaderModel);
-    };
-}
-class boxHeaderModel {
-
-
-    constructor(documentNum,boxHeaderNo, locationToAccount) {
-       // this.documentNum = documentNum;
-        this.boxHeaderNo = boxHeaderNo;
-        this.locationToAccount = locationToAccount;
-        this.boxDetails = new Array();
-    }
-    addDetails(_barcode, _qty, _boxnum) {
-        // this.boxDetails = [];
-        this.boxDetails.push({
-            barcode: _barcode,
-            qty: _qty,
-            boxNum: _boxnum
-        });
-    };
-}
-
-
-var webconfig = {
+const sql = require('mssql');
+var { mongoose } = require('./../server/db/mongoose');
+var { ShippingReceipt } = require('./../server/models/ShippingReceipt');
+const config = {
     server: "erktrdepos03",
     user: "ax",
     password: "465666",
     database: 'AXATAWM_CL',
     connectionString: "Driver={SQL Server Native Client 11.0};Server=#{server}\\sql;Database=#{database};Uid=#{user};Pwd=#{password};",
+    requestTimeout: 22000,
     options: {
-        trustedConnection: false,
+        trustedConnection: true,
         encrypt: false
     },
     pool: {
@@ -51,71 +19,100 @@ var webconfig = {
     }
 };
 
+async function ent009(_documentNum) {
+    let result
+    
+    try {
+        let pool = await sql.connect(config);
+        result = await pool.request()
+            .input('input_parameter', sql.NVarChar, _documentNum)
+            .query('select S09INUM,S09IRTR from ENT009 where S09INUM = @input_parameter')
+        for (var i = 0, len = result.rowsAffected; i < len; i++) {
+            var ENT009 = result.recordset[i];
+            var shippingReceipts = new ShippingReceipt();
+            shippingReceipts.documentNum = ENT009.S09INUM;
+            shippingReceipts.documentDate = ENT009.S09IRTR;
+            shippingReceipts.save()
+                .then( (temp) => {
+                    //console.log(temp);
+                    const data = temp.documentNum;
+                    return Promise.all([data]);
+                }, (e) => {
+                    console.log(e);
+                }).then(async([data]) => {
+                    sql.close();    
+                    const doc = await ent075(data);
+                };
+                // ).then(async([data]) => {
 
-let shippingReceipt;
-let ENT009;
-let ENT0075, ENT0076;
-let boxheader;
-
-var boxheaderObject = (_boxheader) => {
-    return new Promise((resolve, reject) => {
-        var request = new sql.Request();
-        request.query(`select *  from ENT0076  where S76KOLINO = ${_boxheader.boxHeaderNo}`, function (err, result) {
-            if (err) console.log("ENT0076  " + err);
-            for (var k = 0, len = result.rowsAffected; k < len; k++) {
-                ENT0076 = result.recordset[k];
-                _boxheader.addDetails(ENT0076.S76SKU, ENT0076.S76MIKTAR, ENT0076.S76KOLINO);
-            }
-            resolve(_boxheader);
-        });
-    })
-};
-
-var Ent75 = (_shippingReceipt) => {
-    return new Promise((resolve, reject) => {
-        var request = new sql.Request();
-        request.query(`select S75KOLINO,S75SFIRM from ENT0075 where S75IRNO = ${_shippingReceipt.documentNum} group by S75KOLINO,S75SFIRM`, function (err, results) {
-            if (err) console.log("ENT0075  " + err);
+                // });
             
-            for (var i = 0, len = results.rowsAffected; i < len; i++) {
-                ENT0075 = results.recordset[i];
-                // console.log(ENT0075.S75KOLINO);
-                boxheader = new boxHeaderModel(_shippingReceipt.documentNum,ENT0075.S75KOLINO, ENT0075.S75SFIRM);
-                boxheaderObject(boxheader).then(function (_boxheader) {
-                    _shippingReceipt.addboxHeaders(_boxheader);
-                    //console.log(JSON.stringify(_shippingReceipt,null,2));
-                    // fs.appendFile('tmp.json',JSON.stringify(_shippingReceipt,null,2));
-                }
-                );;
-            }
-            fs.appendFile('tmp.json',JSON.stringify(_shippingReceipt,null,2));
-            resolve();
-        });
-    })
+            
+        }
+        sql.close();
+        
+
+    }
+    catch (err) {
+        console.log(err);
+        sql.close();
+    }
 };
 
 
-var Ent09 = () => {
-    let shippingReceipts = [];
-    return new Promise((resolve, reject) => {
-        var request = new sql.Request();
-        // query to the database and get the records
-        request.query("select * from ENT009 where S09INUM in ('30518908','30680025','30521329')", function (err, result) {
-            if (err) console.log("ENT009  " + err);
-            for (var i = 0, len = result.rowsAffected; i < len; i++) {
-                ENT009 = result.recordset[i];
-                shippingReceipt = new shippingReceiptModel(ENT009.S09INUM, ENT009.S09IRTR);
-                Ent75(shippingReceipt).then(function (_shippingReceipt) {
-                   // console.log(JSON.stringify(_shippingReceipt,null,2));
-                  
-                   // console.log(_shippingReceipt);
-                });
-            }
-            //console.log(_shippingReceipt);
-            resolve(shippingReceipts);
-        });
-        // console.log(shippingReceipts[0]);
-    })
+async function ent075(_documentNum) {
+    try {
+        let pool = await sql.connect(config);
+        let result = await pool.request()
+            .input('input_parameter', sql.NVarChar, _documentNum)
+            .query('select S75KOLINO,S75SFIRM from ENT0075 where S75IRNO = @input_parameter group by S75KOLINO,S75SFIRM');
+        console.log(result);
+        for (var i = 0, len = result.rowsAffected; i < len; i++) {
+            var ENT0075 = result.recordset[i];
+            ShippingReceipt.findOne({ documentNum: _documentNum }, function (err, ShippingReceipt) {
+                if (err) throw err;
+                if (ShippingReceipt.locationToAccount !== '') ShippingReceipt.locationToAccount = ENT0075.S75SFIRM;
+                ShippingReceipt.boxHeader.push({ barcode: ENT0075.S75KOLINO });
+                ShippingReceipt.save()
+                    .then((doc) => {
+                        console.log(doc);
+                    });
+            });
+        }
+        sql.close();
+    }
+    catch (err) {
+        console.log(err);
+        sql.close();
+    }
+};
+
+async function ent076(_doc) {
+    try {
+        let boxHeaderNo = _doc.boxHeader[0].barcode;
+        let pool = await sql.connect(config);
+        let result = await pool.request()
+            .input('input_parameter', sql.NVarChar, boxHeaderNo)
+            .query('select *  from ENT0076  where S76KOLINO = @input_parameter');
+        for (var i = 0, len = result.rowsAffected; i < len; i++) {
+            var ENT0076 = result.recordset[k];
+            ShippingReceipt.findById(_doc._id, function (err, _ShippingReceipt) {
+                var subdoc = _ShippingReceipt.boxHeader.id(_doc.boxHeader[0]._id);
+                subdoc.boxDetails.push({ barcode: ENT0076.S76SKU, qty: ENT0076.S76MIKTAR });
+                _ShippingReceipt.save()
+                    .then((doc) => {
+                        //console.log(doc);
+                        //fs.appendFile('tmp.json', JSON.stringify(doc, null, 2));
+                        //resolve(doc);
+                    });
+            });
+        }
+        sql.close();
+
+    } catch (err) {
+        console.log(err);
+
+    }
 };
 
 
@@ -124,18 +121,26 @@ var Ent09 = () => {
 
 
 
+(async function () {
+    try {
+        pool = await sql.connect(config);
+        result = await pool.request()
+            .query('select top(2) * from getOrders');
+        sql.close();
+        for (var i = 0, len = result.rowsAffected; i < len; i++) {
+            const docnum = await ent009(result.recordset[i].S09INUM);
 
 
+        }
+        console.log("done");
 
+    } catch (err) {
+        // ... error checks
+    }
+})();
 
-sql.connect(webconfig, function (err) {
-
-    if (err) console.log(err);
-    // create Request object
-    Ent09().then(function (_shippingReceipts) {
-        _shippingReceipts.forEach(element => {
-            console.log(element);
-        });
-    });
+sql.on('error', err => {
+    console.log(err);
 });
-//mlh
+
+
