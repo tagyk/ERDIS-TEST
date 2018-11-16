db72e113baf8ebaf9697283a0b7643f096282802
Melih Topraksever <melihtopraksever@hotmail.com>
2018-11-16 17:47:59 +0300

    dgsr

--- mssql-client/deneme.js
+++ mssql-client/deneme.js
@@ -1,12 +1,26 @@
 
 var { ShippingReceipt } = require('./../server/models/ShippingReceipt');
+var { mongoose } = require('./../server/db/mongoose');
 
-var ShippingReceipts = ShippingReceipt.find()
-.where('totalQTY').gt(50)
-.sort('totalQTY')
-.select('totalQTY documentNum')
-.exec(function(err,data){
-    if(!err){
+ShippingReceipt.findOne()
+    .where('totalQTY').gt(5)
+    .sort('totalQTY')
+    .select('documentNum')
+    .exec(function (err, data) {
+        if (err) return handleError(err);
         console.log(data);
-    }
-});
+    });
+    ShippingReceipt.find()
+    .where('totalQTY').gt(5)
+    .sort('totalQTY')
+    .select('documentNum totalQTY')
+    .exec(function (err, data) {
+        if (err) return handleError(err);
+        console.log(data);
+    });
+
+
+// (async function () {
+//     let a = await ShippingReceipt.findOne({documentNum: "30678100"});
+//     console.log(a);
+// })()
