faac1c56ba4ec8162443b09ab3f50de7cedec4d2
Melih Topraksever <melihtopraksever@hotmail.com>
2018-12-20 13:39:40 +0300

    v1

--- erdis-wms-ua/server/db/mongoose.js
+++ erdis-wms-ua/server/db/mongoose.js
@@ -7,7 +7,7 @@ mongoose.set('useFindAndModify', false);
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ErdisDev',{
+mongoose.connect('mongodb://localhost:27017/ErdisWmsUA',{
     useCreateIndex: true,
     useNewUrlParser: true 
 });
